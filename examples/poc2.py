8ea7a2aa8c7993d4072049f32fae2ed846a6c5c8
Lukas Riegler <lukasantonriegler@gmail.com>
2023-10-12 20:32:22 +0200

    update package name for second use case example

--- examples/poc2.py
+++ examples/poc2.py
@@ -1,7 +1,7 @@
-from can_broqer import Node
+from canbro import Node
 from can.interface import Bus
-from cantools import database
+from cantools.database import load_file
 
-ecu = Node(name="VCU",bus=Bus("virtul"),database=database.load_file('J1939.dbc') )
+ecu = Node(name="VCU",bus=Bus("virtul"),database=load_file('J1939.dbc') )
 
 
