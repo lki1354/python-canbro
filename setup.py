d34bae4ec4c0ebde7128c21861b6aae6bae98a85
lukas.riegler <lukas.riegler@brusahypower.com>
2023-10-25 21:52:07 +0200

    fix description type to markdown

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open('README.md', 'rb') as readme_file:
 
 setup(
     name='canbro',
-    version='0.0.2',
+    version='0.0.2-alpha',
     description='This package extend the python-can with the broqer package. This provides the functionality to work in a reactive style with can signals and messages.',
     author='Lukas Riegler',
     author_email='lukasantonriegler@gmail.com',
     url='https://github.com/lki1354/python-canbro',
     packages=find_packages(),
+    long_description_content_type='text/markdown',
     long_description=readme,
     install_requires=[
         'python-can>=3.3',
